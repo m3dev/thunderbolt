fed99d0cc2ea3f1e593ce6035b93b274559c692e
6syun9 <6syun9@gmail.com>
2019-10-22 20:37:10 +0900

    add google docstring

--- thunderbolt/thunderbolt.py
+++ thunderbolt/thunderbolt.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import os
 from pathlib import Path
 import pickle
+from typing import Union, List
 
 import boto3
 from boto3 import Session
@@ -11,10 +12,21 @@ from tqdm import tqdm
 
 
 class Thunderbolt():
-    def __init__(self, workspace_directory: str, task_filters=''):
+    def __init__(self, workspace_directory: str = '', task_filters: Union[str, List[str]] = ''):
+        """Thunderbolt init.
+
+        Set the path to the directory or S3.
+
+        Args:
+            workspace_directory: Gokart's TASK_WORKSPACE_DIRECTORY. If not, use $TASK_WORKSPACE_DIRECTORY in os.env.
+            task_filters: Filter for task name.
+                Load only tasks that contain the specified string here. We can also specify the number of copies.
+        """
         self.s3client = None
+        if not workspace_directory:
+            env = os.getenv('TASK_WORKSPACE_DIRECTORY')
+            workspace_directory = env if env else ''
         self.workspace_directory = workspace_directory if workspace_directory.startswith('s3://') else os.path.abspath(workspace_directory)
-        print(self.workspace_directory)
         self.task_filters = [task_filters] if type(task_filters) == str else task_filters
         self.bucket_name = workspace_directory.replace('s3://', '').split('/')[0] if workspace_directory.startswith('s3://') else None
         self.prefix = '/'.join(workspace_directory.replace('s3://', '').split('/')[1:]) if workspace_directory.startswith('s3://') else None
@@ -23,7 +35,7 @@ class Thunderbolt():
         self.tasks = self._get_tasks_from_s3() if workspace_directory.startswith('s3://') else self._get_tasks()
 
     def _get_tasks(self):
-        """Get task parameters."""
+        """Load all task_log from workspace_directory."""
         files = {str(path) for path in Path(os.path.join(self.workspace_directory, 'log/task_log')).rglob('*')}
         tasks = {}
         for i, x in enumerate(tqdm(files)):
@@ -46,7 +58,7 @@ class Thunderbolt():
         return tasks
 
     def _get_tasks_from_s3(self):
-        """Get task parameters from S3."""
+        """Load all task_log from S3"""
         files = self._get_s3_keys([], '')
         tasks = {}
         for i, x in enumerate(tqdm(files)):
@@ -64,16 +76,34 @@ class Thunderbolt():
         return tasks
 
     def _get_s3_keys(self, keys=[], marker=''):
-        """Recursively get Key from S3."""
+        """Recursively get Key from S3.
+
+        Using s3client api by boto module.
+        Reference: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
+
+        Args:
+            keys: The object key to get. Increases with recursion.
+            marker: S3 marker. The recursion ends when this is gone.
+
+        Returns:
+            Object keys from S3. For example: ['hoge', 'piyo', ...]
+        """
         response = self.s3client.list_objects(Bucket=self.bucket_name, Prefix=os.path.join(self.prefix, 'log/task_log'), Marker=marker)
         if 'Contents' in response:
             keys.extend([{'Key': content['Key'], 'LastModified': content['LastModified']} for content in response['Contents']])
-            if 'IsTruncated' in response:
+            if 'Contents' in response and 'IsTruncated' in response:
                 return self._get_s3_keys(keys=keys, marker=keys[-1]['Key'])
         return keys
 
     def get_task_df(self, all_data: bool = False) -> pd.DataFrame:
-        """Get task's pandas data frame."""
+        """Get task's pandas DataFrame.
+
+        Args:
+            all_data: If True, add `task unique hash` and `task log data` to DataFrame.
+
+        Returns:
+            All gokart task infomation pandas.DataFrame.
+        """
         df = pd.DataFrame([{
             'task_id': k,
             'task_name': v['task_name'],
@@ -87,7 +117,15 @@ class Thunderbolt():
         return df[['task_id', 'task_name', 'last_modified', 'task_params']]
 
     def load(self, task_id: int) -> list:
-        """Load File."""
+        """Load File using gokart.load.
+
+        Args:
+            task_id: Specify the ID given by Thunderbolt, Read data into memory.
+                Please check `task_id` by using Thunderbolt.get_task_df.
+
+        Returns:
+            The return value is List. This is because it may be divided when dumping by gokart.
+        """
         return [
             gokart.target.make_target(file_path=os.path.join(os.path.dirname(self.workspace_directory), x)).load()
             for x in self.tasks[task_id]['task_log']['file_path']
